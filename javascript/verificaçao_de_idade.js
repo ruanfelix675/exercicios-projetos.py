ed7397da55098ac195d1c470f70b4be9f6cca0ab
ruanfelix675 <ruanfeliz7777@gmail.com>
2026-03-24 16:51:04 -0300

    Create verificaçao_de_idade.js

---
+++ javascript/verificaçao_de_idade.js
@@ -0,0 +1,9 @@
+let idade = 18
+let nome = 'ruan'
+if (idade < 12) {
+  console.log(nome + " - Muito novo")
+} else if (idade >= 18) {
+  console.log(nome + " - Acesso liberado")
+} else {
+  console.log(nome + " - Acesso negado")
+}
