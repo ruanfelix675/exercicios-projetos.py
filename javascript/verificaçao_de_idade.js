0315a75de203a97b99758989b5377cf01ddb5c57
ruan_void7777 <ruanfeliz7777@gmail.com>
2026-03-27 15:05:12 -0300

    Delete javascript/verificaçao_de_idade.js

--- javascript/verificaçao_de_idade.js
+++
@@ -1,9 +0,0 @@
-let idade = 18
-let nome = 'ruan'
-if (idade < 12) {
-  console.log(nome + " - Muito novo")
-} else if (idade >= 18) {
-  console.log(nome + " - Acesso liberado")
-} else {
-  console.log(nome + " - Acesso negado")
-}
