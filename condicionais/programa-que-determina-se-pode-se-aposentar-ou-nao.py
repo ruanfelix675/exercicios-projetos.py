0ea5d731c2e6c3c58bbc2e0996a5ce7271fbeee5
ruan_void7777 <ruanfeliz7777@gmail.com>
2026-03-30 13:10:57 -0300

    Create programa-que-determina-se-pode-se-aposentar-ou-nao.py

---
+++ condicionais/programa-que-determina-se-pode-se-aposentar-ou-nao.py
@@ -0,0 +1,6 @@
+idade = int(input('qual e sua idade? '))
+tempo_trabalhado = int(input('digite quantos anos vc  trabalhou? '))
+if idade >= 65 and tempo_trabalhado >= 60 :
+    print('pode se aposentar')
+else:
+    print('n pode se aposentar')
