39994feecc8a3f65bc9b5b1ae9106c6c5ed7607b
ruan_void7777 <ruanfeliz7777@gmail.com>
2026-03-31 15:36:39 -0300

    Create registrando-sexo.py

---
+++ while/registrando-sexo.py
@@ -0,0 +1,7 @@
+sexo = input('digite seu sexo f pra feminino e m pra masculino ')
+while sexo == 'm' and sexo == 'f':
+   sexo = input('digite seu sexo ')
+if sexo == 'm' :
+    print('sexo masculino registrado com sucesso ')
+elif sexo == 'f':
+    print('sexo feminino registrado com sucesso')
